1ad529bf27f7625eee0a5dfedd9f5eaec4667868
jaimemin <jaimemin@gmail.com>
2020-12-15 00:52:43 +0900

    authentication setting

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,6 +2,8 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,24 +25,37 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}1111")
+                .roles("USER");
+
+        auth.inMemoryAuthentication()
+                .withUser("sys")
+                .password("{noop}1111")
+                .roles("SYS", "USER");
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}1111")
+                .roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 인가 정책
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
 
         // 인증 정책
         http
-                .formLogin();
-
-        // 세션 고정 공격 보호 설정
-        http
-            .sessionManagement()
-            .sessionFixation().changeSessionId()
+                .formLogin()
         ;
     }
 }
