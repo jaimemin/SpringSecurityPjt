7adf90c59870cf01cd08077978ae613582c1fe38
jaimemin <jaimemin@gmail.com>
2020-12-23 01:12:31 +0900

    SecurityContextHolder

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,37 +1,28 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
-@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/admin/**")
-                .authorizeRequests()
-                .anyRequest().authenticated()
-        .and()
-                .httpBasic();
-    }
-}
-
-@Configuration
-@Order(1)
-class SecurityConfig2 extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+        // 인가 정책
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin();
+                .anyRequest().authenticated();
+
+        // 인증 정책
+        http
+                .formLogin()
+        ;
+
+        // 메인 쓰레드와 자식 쓰레드에 관하여 동일한 SecurityContext 유지
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,38 +1,44 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        // SecurityContextHolder에서 꺼내오는 방식
+        Authentication authentication
+                = SecurityContextHolder.getContext().getAuthentication();
+
+        // 세션에서 꺼내오는 방식
+        SecurityContext context
+                = (SecurityContext)session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication2 = context.getAuthentication();
+
         return "home";
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "user";
-    }
+    @GetMapping("/thread")
+    public String thread() {
+        // Thread 간 SecurityContext 확인
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication
+                                = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+        ).start();
 
-    @GetMapping("/admin/pay")
-    public String adminPay() {
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin() {
-        return "admin";
-    }
-
-    @GetMapping("/denied")
-    public String denied() {
-        return "Access is denied";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+        return "thread";
     }
 }
